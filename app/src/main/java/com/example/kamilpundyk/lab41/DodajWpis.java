ba5abdab3291c66019072d52dc02024420c20788
xOx1141 <kpundyk@gmail.com>
2018-04-19 11:23:15 +0200

    New stuff

---
+++ app/src/main/java/com/example/kamilpundyk/lab41/DodajWpis.java
@@ -0,0 +1,13 @@
+package com.example.kamilpundyk.lab41;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+}
--- app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
+++ app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kamilpundyk.lab41;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,DodajWpis.class);
+        startActivityForResult(intencja,1);
     }
 }
