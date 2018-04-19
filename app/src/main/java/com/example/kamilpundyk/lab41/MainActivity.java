b3fe78d57e910ae6f5aff9d648588978ab3cfe68
xOx1141 <kpundyk@gmail.com>
2018-04-19 11:13:58 +0200

    Nie podobal mi sie

--- app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
+++ app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.kamilpundyk.lab41;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values = new String[] {"Honda","Kawasaki","Suzuki","Ducati"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
     }
 }
