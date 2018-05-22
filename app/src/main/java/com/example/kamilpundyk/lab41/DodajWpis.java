b0e39cbc0209d73a08a2c1194e5b9456b6ce6ca1
xOx1141 <kpundyk@gmail.com>
2018-05-23 01:28:40 +0200

    Last Version

--- app/src/main/java/com/example/kamilpundyk/lab41/DodajWpis.java
+++ app/src/main/java/com/example/kamilpundyk/lab41/DodajWpis.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -12,13 +14,20 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+        ArrayAdapter gatunki = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, new String[]{"Pies", "Kot", "Rybki"});
+        Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
+        gatunek.setAdapter(gatunki);
     }
     public void wyslij(View v){
-        EditText kontrolka = (EditText)findViewById(R.id.editText);
-        String pole = kontrolka.getText().toString();
+        EditText kolor = (EditText) findViewById(R.id.kolor);
+        EditText wielkosc = (EditText) findViewById(R.id.wielkosc);
+        EditText opis = (EditText) findViewById(R.id.opis);
+        Spinner gatunek = (Spinner) findViewById(R.id.gatunek);
+
+        Animal zwierze = new Animal(gatunek.getSelectedItem().toString(), kolor.getText().toString(), Float.valueOf(wielkosc.getText().toString()), opis.getText().toString());
         Intent intencja = new Intent();
-        intencja.putExtra("wpis",pole);
-        setResult(RESULT_OK,intencja);
+        intencja.putExtra("nowy", zwierze);
+        setResult(RESULT_OK, intencja);
         finish();
     }
 
--- app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
+++ app/src/main/java/com/example/kamilpundyk/lab41/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.kamilpundyk.lab41;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,7 +16,9 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
-    private ArrayAdapter adapter;
+    private SimpleCursorAdapter adapter;
+    MySQLite db = new MySQLite(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         String[] values = new String[] {"Honda","Kawasaki","Suzuki","Ducati"};
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
-        this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,this.target);
+        this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(), new String[] {"_id", "gatunek"}, new int[]{android.R.id.text1, android.R.id.text2}, SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
     }
@@ -46,8 +49,9 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==1 && resultCode==RESULT_OK)
         {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
+            Animal nowy = (Animal) extras.getSerializable("nowy");
+            this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
     }
